bbbce785e4fdbdf4d670760c70428c85abb8a388
Jessica Mattick <jmatty1661@gmail.com>
2021-01-06 02:10:39 -0600

    add jump to player controller

--- 2020game/Assets/Scripts/PlayerController.cs
+++ 2020game/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float forwardForce = 10000f;
+    public float forwardForce = 500f;
+    public float jumpForce = 500f;
+    bool isGrounded = true;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -27,6 +29,20 @@ public class PlayerController : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             rb.AddForce(direction * forwardForce * Time.deltaTime);
-        }        
+        }      
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isGrounded = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 2020game
+
+Game can be played here:
+https://jmattick.github.io/2020game/Builds/index.html
+
+### Abstract
+
+### Platform
+
+- Desktop
+
+### Development Tools / Languanges
+
+- Unity 2019.4.12f1
+- C#
+
+### SDKS
+
+### APIS
+
+### Assets
+
+### Video
+
