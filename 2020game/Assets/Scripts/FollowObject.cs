7fabee9ec0483e30285241e9cc7c69b5955c41c3
Jessica Mattick <jmatty1661@gmail.com>
2021-01-06 00:24:23 -0600

    create basic level and player controller

---
+++ 2020game/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform followObject;
+    Vector3 offset;
+
+    private void Start()
+    {
+        //Calculate offset between camera and object
+        offset = Camera.main.transform.position - followObject.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Set camera position as object moves
+        Camera.main.transform.position = followObject.position + offset;
+        Camera.main.ScreenToWorldPoint(followObject.position);
+    }
+}
---
+++ 2020game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forwardForce = 10000f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get rigidbody
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        //add forces
+        if (direction.magnitude >= 0.1f)
+        {
+            rb.AddForce(direction * forwardForce * Time.deltaTime);
+        }        
+    }
+}
